74097c523b6c8653888bff55bee5eba858082bee
Simo Mazil <mohamed.mazil@gmail.com>
2019-12-05 23:26:40 +0100

    add API folder with some const data

---
+++ src/api/API.js
@@ -0,0 +1,14 @@
+export const messages = [
+  {
+    message: "This will be great for business reports. I will definitely download this.",
+    status: "public"
+  },
+  {
+    message: "This will be great for business reports. I will definitely download this.",
+    status: "public"
+  },
+  {
+    message: "This will be great for business reports. I will definitely download this.",
+    status: "public"
+  }
+]
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Comment, Label, Icon, Divider } from 'semantic-ui-react'
 
-const Message = ({name, message, status}) => {
+const Message = ({message, status}) => {
   return (
     <Comment>
       <Comment.Content>
-        <Comment.Author>Tom Lukic</Comment.Author>
         <Comment.Text>
           This will be great for business reports. I will definitely download this.
         </Comment.Text>
